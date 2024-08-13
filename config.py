a3f53ac02e7dfd6b7f7136330181504ca796bfcc
Administrativo SMED <leobarbosapinturas@gmail.com>
2024-08-13 12:21:59 -0300

    feat: Adiciona arquivo de configuração das variáveis de ambiente

---
+++ config.py
@@ -0,0 +1,12 @@
+from dotenv import load_dotenv
+import os
+
+# Carregar variáveis do .env
+load_dotenv()
+
+
+# Acessar variáveis de ambiente
+DB_HOST = os.getenv('DB_HOST')
+DB_USER = os.getenv('DB_USER')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
+DB_NAME = os.getenv('DB_NAME')
