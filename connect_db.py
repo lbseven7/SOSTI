562a7c911b226429c8c95a6d76cea959e6a2743d
lbseven7 <leodevcode@gmail.com>
2024-08-13 12:58:25 -0300

    feat: Cria função get_data para obter os dados d banco e exibir em tela

--- connect_db.py
+++ connect_db.py
@@ -1,6 +1,8 @@
 import os
 import mysql.connector
 from dotenv import load_dotenv
+import streamlit as st
+import pandas as pd
 
 # Carrega as variáveis de ambiente do arquivo .env
 load_dotenv()
@@ -11,23 +13,43 @@ db_user = os.getenv('DB_USER')
 db_pass = os.getenv('DB_PASS')
 db_name = os.getenv('DB_NAME')
 
-# Conectando ao banco de dados
-try:
-    connection = mysql.connector.connect(
-        host=db_host,
-        user=db_user,
-        password=db_pass,
-        database=db_name
-    )
+# Função para obter dados do banco de dados
+def get_data():
+    try:
+        connection = mysql.connector.connect(
+            host=db_host,
+            user=db_user,
+            password=db_pass,
+            database=db_name
+        )
 
-    if connection.is_connected():
-        print("Conexão com o banco de dados foi bem-sucedida")
-        # Aqui você pode executar suas queries
+        if connection.is_connected():
+            cursor = connection.cursor()
+            cursor.execute("SELECT * FROM escola")
+            
+            # Recuperando os nomes das colunas
+            colunas = [desc[0] for desc in cursor.description]
+            
+            # Recuperando todos os registros retornados pela query
+            rows = cursor.fetchall()
 
-except mysql.connector.Error as err:
-    print(f"Erro ao conectar ao banco de dados: {err}")
+            # Criando um DataFrame com os dados
+            df = pd.DataFrame(rows, columns=colunas)
+            
+            return df
 
-finally:
-    if connection.is_connected():
-        connection.close()
-        print("Conexão encerrada")
+    except mysql.connector.Error as err:
+        st.error(f"Erro ao conectar ao banco de dados: {err}")
+
+    finally:
+        if connection.is_connected():
+            cursor.close()
+            connection.close()
+
+# Configuração do aplicativo Streamlit
+st.title('Dados da Tabela Escola')
+
+# Obtendo e exibindo os dados
+data = get_data()
+if data is not None:
+    st.write(data)
